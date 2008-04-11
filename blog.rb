e12d608c67485f9cc4c40fa92e97772ffe929151
Carlos Gabaldon <carlos@carlos-gabaldons-computer.local>
2008-04-10 21:30:32 -0700

    initial commit

---
+++ blog.rb
@@ -0,0 +1,153 @@
+require 'rubygems'
+require 'sinatra'
+require 'data_mapper'
+
+
+### DB SETUP ###
+
+DataMapper::Database.setup({
+  :adapter  => 'sqlite3',
+  :host     => 'localhost',
+  :username => '',
+  :password => '',
+  :database => 'db/my_way_development'
+})
+
+### MODELS ###
+
+class Article < DataMapper::Base
+  has_many :comments
+  has_many :tags
+  property :title, :text
+  property :text, :text
+  property :posted_by, :string
+  property :permalink, :text
+  property :created_at, :datetime
+  property :updated_at, :datetime
+  
+  def short_text
+    text_len = 50
+    if self.text.length > text_len
+      "#{self.text[0, text_len]}.. <a href='/article/#{self.permalink}'> more >>"
+    else
+      self.text
+    end
+  end
+  
+end
+  
+class Comment < DataMapper::Base
+  belongs_to :article
+  property :posted_by, :string
+  property :email, :string
+  property :url, :string
+  property :body, :text
+end
+
+class Tag < DataMapper::Base
+  has_many :articles
+  property :name, :text
+  property :count, :integer
+end
+
+database.table_exists?(Article) or database.save(Article)
+database.table_exists?(Comment) or database.save(Comment)
+database.table_exists?(Tag) or database.save(Tag)
+
+### CONTROLLER ACTIONS
+
+layout 'layout.haml'
+
+static '/css', 'css'
+
+## LIST ARTICLES ##
+get '/articles' do
+  @articles = Article.all :limit => 10, 
+                          :order => 'created_at desc'                       
+  view :articles
+end
+
+
+## VIEW ARTICLE ##
+get '/article/:permalink' do
+  @article = Article.find :first,
+                          :permalink => params[:permalink] 
+  view :article
+end
+
+
+## NEW ARTICLE ##
+get '/articles/new' do
+  view :article_new
+end
+
+post '/articles/create' do
+
+  @article = Article.new :title     => params[:article_title],
+                         :text      => params[:article_text],
+                         :posted_by => params[:article_posted_by],
+                         :permalink => create_permalink(params[:article_title])
+  if @article.save 
+    redirect "/article/#{@article.permalink}"
+  else
+    redirect "/articles"
+  end
+  
+end
+
+
+## EDIT ARTICLE ##
+get 'article/edit/:permalink' do
+  ## todo
+end
+
+post 'article/update' do
+  ## todo
+end
+
+private
+
+def view(view)
+  @content = haml view
+  haml :layout
+end
+
+def create_permalink(string)
+  string = string.strip
+  string = string.tr(' ', '_')
+  string = string.tr("'", "")
+  string = string.tr("$", "")
+  string = string.tr("&", "")
+  string = string.tr("<", "")
+  string = string.tr(">", "")
+  string = string.tr("*", "")
+  string = string.tr("@", "")
+  string = string.tr(".", "")
+  string = string.tr(":", "")
+  string = string.tr("|", "")
+  string = string.tr("~", "")
+  string = string.tr("`", "")
+  string = string.tr("(", "")
+  string = string.tr(")", "")
+  string = string.tr("%", "")
+  string = string.tr("#", "")
+  string = string.tr("^", "")
+  string = string.tr("?", "")
+  string = string.tr("/", "")
+  string = string.tr("{", "")
+  string = string.tr("}", "")
+  string = string.tr(",", "")
+  string = string.tr(";", "")
+  string = string.tr("!", "")
+  string = string.tr("+", "")
+  string = string.tr("=", "")
+  string = string.tr("-", "_")
+end
+
+
+
+
+
+
+
+
