ac013ca389eaaf60315e91590f1cfe7d8bba6d7f
Carlos Gabaldon <carlos@carlos-gabaldons-computer.local>
2008-04-20 20:14:21 -0700

    Adding tag support

--- blog.rb
+++ blog.rb
@@ -13,17 +13,21 @@ DataMapper::Database.setup({
   :database => 'db/my_way_development'
 })
 
+
+$LOG = Logger.new('log/my_way.log')
+$LOG.level = Logger::INFO
+
 ### MODELS ###
 
 class Article < DataMapper::Base
   has_many :comments
   has_many :tags
   
-  property :id, :integer, :key => true
-  property :title, :text
-  property :text, :text
-  property :posted_by, :string
-  property :permalink, :text
+  property :id,         :integer, :key => true
+  property :title,      :text
+  property :text,       :text
+  property :posted_by,  :string
+  property :permalink,  :text
   property :created_at, :datetime
   property :updated_at, :datetime
   
@@ -75,15 +79,31 @@ end
 class Comment < DataMapper::Base
   belongs_to :article
   property :posted_by, :string
-  property :email, :string
-  property :url, :string
-  property :body, :text
+  property :email,     :string
+  property :url,       :string
+  property :body,      :text
 end
 
 class Tag < DataMapper::Base
   has_many :articles
   property :name, :text
   property :count, :integer
+  
+  class << self
+    def build(options)
+      name = options[:name]
+      article = options[:article]
+      
+      tag = Tag.find(:first, :name => name)
+      if tag
+        tag.count += 1
+        article.tags << tag
+      else
+          article.tags.create(:name => name, :count => 1)
+      end
+    
+    end
+  end
 end
 
 database.table_exists?(Article) or database.save(Article)
@@ -100,16 +120,22 @@ end
 ## LIST ARTICLES ##
 get '/articles' do
   
-  @articles = Article.all :limit => 10, 
-                          :order => 'created_at desc'                       
+  @articles = Article.all(:limit => 10, 
+                          :order => 'created_at desc')  
+                          
+  $LOG.info("GET '/articles' @articles.length => #{@articles.length.to_s}")     
+                 
   view :articles
 end
 
 
 ## VIEW ARTICLE ##
 get '/article/:permalink' do
-  @article = Article.find :first,
-                          :permalink => params[:permalink] 
+  @article = Article.find(:first,
+                          :permalink => params[:permalink])
+                          
+  $LOG.info("GET '/article/#{params[:permallink]}' @article.title => #{@article.title}")
+  
   view :article
 end
 
@@ -121,10 +147,21 @@ end
 
 post '/articles/create' do
 
-  @article = Article.new :title     => params[:article_title],
+  @article = Article.new(:title     => params[:article_title],
                          :text      => params[:article_text],
                          :posted_by => params[:article_posted_by],
-                         :permalink => create_permalink(params[:article_title])
+                         :permalink => clean(params[:article_title]))
+       
+  tags_param = params[:article_tags]    
+  
+  $LOG.info("POST 'articles/create' 
+            params[:article_tags] => #{@article.title}")
+          
+  if tags_param
+    tags_param.each(",") do |tag_param| 
+      Tag.build(:article => @article, :name => clean(tag_param))
+    end
+  end
   
   if @article.valid?
     if @article.save 
@@ -141,18 +178,21 @@ end
 
 ## EDIT ARTICLE ##
 get '/article/edit/:permalink' do
-  @article = Article.find :first,
-                          :permalink => params[:permalink] 
+  @article = Article.find(:first,
+                          :permalink => params[:permalink])
+                          
+  $LOG.info("GET '/article/edit#{params[:permallink]}' @article.title => #{@article.title}")
+  
   view :article_edit
 end
 
 post '/article/update/:permalink' do
-  @article = Article.find :first,
-                          :permalink => params[:permalink]
+  @article = Article.find(:first,
+                          :permalink => params[:permalink])
   if @article
-    @article.title = params[:article_title]
-    @article.text = params[:article_text]
-    @article.posted_by = params[:article_posted_by]
+    @article.title      = params[:article_title]
+    @article.text       = params[:article_text]
+    @article.posted_by  = params[:article_posted_by]
     @article.updated_at = Time.now
     
     if @article.save
@@ -168,14 +208,13 @@ end
 helpers do
   def view(view)
     haml view
-    #erb view
   end
 end
 
 
 
 
-def create_permalink(string)
+def clean(string)
   string = string.strip
   string = string.tr(' ', '_')
   string = string.tr("'", "")
